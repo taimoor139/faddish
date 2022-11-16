ae17d749fc63a5ef2ac3d8d6d4d297816ec3a43f
taimoor ahmad <taimoorahmad139@gmail.com>
2022-11-16 18:04:27 +0500

    login working

--- storemanager.faddishbuilder.com/app/Http/Controllers/Api/IntegrationController.php
+++ storemanager.faddishbuilder.com/app/Http/Controllers/Api/IntegrationController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class IntegrationController extends Controller
 {
     public function addUser(Request $request){
+
+        $user = User::create([
+            'name' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'type' => 'Owner',
+            'lang' => !empty($settings['default_language']) ? $settings['default_language'] : 'en',
+            'avatar' => 'avatar.png',
+            'plan' => Plan::first()->id,
+            'created_by' => 1,
+        ]);
+
+        return $user;
         return "working";
     }
 }
